4eea5cc0e80424d838d0952386e8f0faff8ea922
Julien Smeets <j.smeets@yipyip.nl>
2019-07-23 16:47:18 +0200

    Add Ecto custom validators

---
+++ test/ecto/validators_test.exs
@@ -0,0 +1,29 @@
+defmodule PhoenixApiToolkit.Ecto.ValidatorsTest do
+  use ExUnit.Case, async: true
+
+  import PhoenixApiToolkit.Ecto.Validators
+  import Ecto.Changeset
+
+  @pdf_signature "255044462D" |> Base.decode16!()
+  @png_signature "89504E470D0A1A0A" |> Base.decode16!()
+  @png_file "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
+  @gif_file "R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+  @orderable ["first_name", "last_name"]
+  @schema %{
+    first_name: :string,
+    last_name: :string,
+    last_name_prefix: :string,
+    order_by: :string,
+    file: :string
+  }
+
+  def changeset(changes \\ %{}) do
+    {%{}, @schema} |> cast(changes, [:first_name, :last_name, :order_by, :file])
+  end
+
+  doctest PhoenixApiToolkit.Ecto.Validators
+
+  test "validate_upload passes through no-change" do
+    assert changeset() |> validate_upload(:file, @pdf_signature) == changeset()
+  end
+end
