d995febb9745c20983309e5b47b0c622a15af113
Julien Smeets <j.smeets@yipyip.nl>
2019-08-05 10:05:37 +0200

    Bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixApiToolkit.MixProject do
   def project do
     [
       app: :phoenix_api_toolkit,
-      version: "0.2.0-alpha",
+      version: "0.3.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
