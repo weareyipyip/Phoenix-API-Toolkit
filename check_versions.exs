170c695bc2c0d812c8d388f9710bff21f42656ba
J. Smeets <j.smeets@yipyip.nl>
2019-08-10 10:57:12 +0200

    Migrate to new GitHub Actions yaml format

--- check_versions.exs
+++ check_versions.exs
@@ -1,11 +1,9 @@
-[readme_path] = System.argv()
-
 # Get the current git tag and expected version from the CI system
 "refs/tags/v" <> expected_version =
   System.get_env("GITHUB_REF", "Environment variable GITHUB_REF not found")
 
 # Read the readme
-readme = File.read!(readme_path)
+readme = File.read!("README.md")
 
 # capture 0.3.0-alpha from {:phoenix_api_toolkit, "~> 0.3.0-alpha"}
 readme_version_regex = ~r/:phoenix_api_toolkit,\s\"~>\s(?<version>.*)\"/
