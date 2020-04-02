b5a0437a1c26321447024a0c275b135952c19dd9
J. Smeets <j.smeets@yipyip.nl>
2020-04-02 11:58:48 +0200

    Update deps, beta 2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixApiToolkit.MixProject do
   def project do
     [
       app: :phoenix_api_toolkit,
-      version: "0.12.0-beta.1",
+      version: "0.12.0-beta.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
