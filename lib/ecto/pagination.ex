e05c555a2f7bf78c487cb31f014cebb5d005af5d
Louis Matthijssen <l.matthijssen@yipyip.nl>
2020-08-14 12:12:53 +0200

    Apply pagination feedback

--- lib/ecto/pagination.ex
+++ lib/ecto/pagination.ex
@@ -7,30 +7,32 @@ defmodule PhoenixApiToolkit.Ecto.Pagination do
   @doc """
   Paginate the query by specifying a limit and/or offset.
 
+  Note: the primary model must have a single unique column to use this method.
   Note: must be called after all filters have been added to the query.
+  Note: when querying a single table (e.g. no joins are being used), Ecto's limit/offset is faster.
 
   The limit and/or offset will be applied to the primary model.
 
       iex> base_query()
-      ...> |> paginate(%{})
+      ...> |> paginate_binding(%{}, :user)
       #Ecto.Query<from u0 in "users", as: :user>
 
       iex> base_query()
-      ...> |> paginate(%{limit: 10})
+      ...> |> paginate_binding(%{limit: 10}, :user)
       #Ecto.Query<from u0 in "users", as: :user, where: u0.id in subquery(from u0 in "users",
         as: :user,
         limit: ^10,
         select: u0.id)>
 
       iex> base_query()
-      ...> |> paginate(%{offset: 10})
+      ...> |> paginate_binding(%{offset: 10}, :user)
       #Ecto.Query<from u0 in "users", as: :user, where: u0.id in subquery(from u0 in "users",
         as: :user,
         offset: ^10,
         select: u0.id)>
 
       iex> base_query()
-      ...> |> paginate(%{limit: 10, offset: 10})
+      ...> |> paginate_binding(%{limit: 10, offset: 10}, :user)
       #Ecto.Query<from u0 in "users", as: :user, where: u0.id in subquery(from u0 in "users",
         as: :user,
         limit: ^10,
@@ -40,7 +42,7 @@ defmodule PhoenixApiToolkit.Ecto.Pagination do
       iex> query = base_query()
       ...> |> join(:left, [u], e in assoc(u, :email_addresses), as: :email_addresses)
       ...> |> where([email_addresses: e], e.is_verified)
-      ...> |> paginate(%{offset: 20, limit: 10})
+      ...> |> paginate_binding(%{offset: 20, limit: 10}, :user)
       #Ecto.Query<from u0 in "users", as: :user, left_join: e1 in assoc(u0, :email_addresses), as: :email_addresses, where: e1.is_verified, where: u0.id in subquery(from u0 in "users",
         as: :user,
         left_join: e1 in assoc(u0, :email_addresses),
@@ -50,25 +52,25 @@ defmodule PhoenixApiToolkit.Ecto.Pagination do
         offset: ^20,
         select: u0.id)>
   """
-  @spec paginate(Ecto.Query.t(), map) :: Ecto.Query.t()
-  def paginate(query, filters) do
+  @spec paginate_binding(Ecto.Query.t(), map, atom) :: Ecto.Query.t()
+  def paginate_binding(query, filters, named_binding) do
     offset = Map.get(filters, :offset)
     limit = Map.get(filters, :limit)
 
-    paginate(query, offset, limit)
+    paginate_binding(query, offset, limit, named_binding)
   end
 
-  defp paginate(query, nil, nil), do: query
+  defp paginate_binding(query, nil, nil, _), do: query
 
-  defp paginate(query, offset, limit) do
+  defp paginate_binding(query, offset, limit, named_binding) do
     ids_query =
       query
       |> maybe_filter_offset(offset)
       |> maybe_filter_limit(limit)
-      |> select([m], m.id)
+      |> select([{^named_binding, m}], m.id)
 
     query
-    |> where([m], m.id in subquery(ids_query))
+    |> where([{^named_binding, m}], m.id in subquery(ids_query))
   end
 
   defp maybe_filter_offset(query, nil), do: query
