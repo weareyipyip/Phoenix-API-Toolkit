6348329156dbb5486302a0e0941ca73a4c28043d
Louis Matthijssen <l.matthijssen@yipyip.nl>
2022-12-06 11:11:51 +0100

    Fix CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixApiToolkit.MixProject do
     [
       app: :phoenix_api_toolkit,
       version: "0.0.0+development",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: """
